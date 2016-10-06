dbaec4ca3d04da1c84287744b484b719e3eb5466
ankitkcs <kcsmac@KCS-iMac.local>
2016-10-06 17:39:53 +0530

    committe

--- CIDEMO/ViewController.m
+++ CIDEMO/ViewController.m
@@ -25,7 +25,7 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    //Demo one
+    //Demo two
 }
 
 - (IBAction)btnClick:(id)sender {
